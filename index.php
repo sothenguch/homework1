1b66ddc9384ac2a91a4b992a95944261881426a8
thengboz <thengboz@gmail.com>
2017-09-07 06:06:32 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+  <title>Home</title>
+</head>
+
+<body>
+  <body>
+    
+    <center><font size="6"><b>Computer Virus</b></font></center>
+    <div>
+      <ul class = "list">
+        <li class="active"><b href="index.php" alt="Home">Main Page</b></li>
+        <li><a href="definition.html" alt="Definition">Definition</a></li>
+        <li><a href="how.html" alt="How">How?</a></li>
+        <li><a href="statistic.html" alt="Statistic">Statistic</a></li>
+      </ul>
+    </div>
+    <!--main content-->
+    <p>
+    <img src="img/virus.jpg" width="300" height="200">
+    This website will cover the information about computer virus. We will cover what is virus, how it infected computer and satistic.
+    </p>
+    
+    
+  </body>
+  <footer>
+    This is the Link to the source:
+    <a href="https://us.norton.com/internetsecurity-malware-what-is-a-computer-virus.html" alt="Statistic">Click Me!</a>
+  </footer>
+</html>
